f0362254f3c209ade000fccb87503b8fef79624c
Matthew Tucker <matt@joinpaladin.com>
2021-02-13 16:08:24 -0500

    header padding

--- components/header.js
+++ components/header.js
@@ -3,10 +3,12 @@ import { SITE_NAME } from "../lib/constants";
 
 export default function Header() {
   return (
-    <h3>
-      <Link href="/">
-        <a>{SITE_NAME}</a>
-      </Link>
-    </h3>
+    <header>
+      <h3>
+        <Link href="/">
+          <a>{SITE_NAME}</a>
+        </Link>
+      </h3>
+    </header>
   );
 }
